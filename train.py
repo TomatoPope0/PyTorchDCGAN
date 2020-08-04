7e4fa5a188300b471645c0b8ea9c7ab7d09298bd
TomatoPope0 <tomatopope0@gmail.com>
2020-08-04 16:21:35 +0900

    Normalize data

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import torch
 import torchvision
+import torchvision.transforms as transforms
 
 # Hyperparameters
 BATCH_SIZE = 128
@@ -8,7 +9,14 @@ NUM_WORKERS = 4
 mnist_train = torchvision.datasets.MNIST(
     "../Datasets/MNIST_PyTorch",
     train=True,
-    transform=torchvision.transforms.ToTensor()
+    transform=transforms.Compose([
+        transforms.ToTensor(),
+        # ToTensor() returns [0, 1]-ranged tensor.
+        # Normalize() performs x' = (x - m)/s for every channel
+        # Thus, Normalize((0.5), (0.5)) scales tensor to [-1, 1]
+        # as (0-0.5)/0.5=-1, (1-0.5)/0.5=1
+        transforms.Normalize((0.5), (0.5))
+    ])
 )
 mnist_loader = torch.utils.data.DataLoader(
     mnist_train,
