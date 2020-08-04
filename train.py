b736d1582e85ae7b52d94405ad2be62325e27aaa
TomatoPope0 <tomatopope0@gmail.com>
2020-08-04 16:51:59 +0900

    Initialize optimizers

--- train.py
+++ train.py
@@ -5,10 +5,16 @@ import torchvision.transforms as transforms
 from dcgan import Generator, Discriminator
 
 # Hyperparameters
+## Data Loader
 BATCH_SIZE = 128
 NUM_WORKERS = 4
+## Weight Initialization
 WEIGHT_MEAN = 0
 WEIGHT_STD = 0.02
+## Adam Optimizer
+LEARNING_RATE = 0.0002
+BETA1 = 0.5
+BETA2 = 0.99
 
 # Device Initialization
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -41,12 +47,24 @@ def init_weights(m):
     # elif classname.find('BatchNorm') != -1:
     #    pass
             
-
 G = Generator().to(device)
 G.apply(init_weights)
 
 D = Discriminator().to(device)
 D.apply(init_weights)
+
+# Assume BCELoss, since it's based on GAN (Goodfellow, 2014)
+criterion = nn.BCELoss()
+optimizerG = torch.optim.Adam(
+    G.parameters,
+    lr=LEARNING_RATE,
+    betas=[BETA1, BETA2]
+)
+optmizerD = torch.optim.Adam(
+    D.parameters,
+    lr=LEARNING_RATE,
+    betas=[BETA1, BETA2]
+)
 
 # On windows, using multi-worker DataLoader outside of
 # if causes recursive process creation (runtime error)
